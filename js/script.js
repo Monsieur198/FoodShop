fbee695293ba23f58d698ce2debcafeb08da135c
Sergey <mons198x@gmail.com>
2021-04-23 19:54:36 +0300

    fetch formData

--- js/script.js
+++ js/script.js
@@ -225,28 +225,29 @@ document.addEventListener('DOMContentLoaded', () => {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-
-            request.setRequestHeader('Content-type', 'aplication/json');
             const formData = new FormData(form);
 
-            const object = {};
-            formData.forEach((value, key) => object[key] = value);
+            // const object = {};
+            // formData.forEach((value, key) => object[key] = value);
 
-            const json = JSON.stringify(object);
+            // const json = JSON.stringify(object);
 
-            request.send(json);
+            fetch('server.php', {
+                method: 'POST',
+                // headers: {
+                //     'Content-type': 'aplication/json'
+                // }
+                body: formData
+            }).then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
 
-            request.addEventListener('load', () => {
-                if (request.status === 200){
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
             });
         });
     }
